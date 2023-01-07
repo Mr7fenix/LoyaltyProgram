6f887cf16bd1f3483fc06d2ed7a7c1ad4968ef14
Mr7fenix <mr7fenix@gmail.com>
2023-01-07 13:09:06 +0100

    Initial commit

---
+++ loyaltyprogrambackend/src/main/java/it/cs/unicam/dicygroup/loyaltyprogrambackend/LoyaltyprogrambackendApplication.java
@@ -0,0 +1,13 @@
+package it.cs.unicam.dicygroup.loyaltyprogrambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyprogrambackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyaltyprogrambackendApplication.class, args);
+    }
+
+}
---
+++ loyaltyprogrambackend/src/test/java/it/cs/unicam/dicygroup/loyaltyprogrambackend/LoyaltyprogrambackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cs.unicam.dicygroup.loyaltyprogrambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyprogrambackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
