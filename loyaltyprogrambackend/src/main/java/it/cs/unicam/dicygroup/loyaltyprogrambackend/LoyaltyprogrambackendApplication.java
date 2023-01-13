65d7145d51fde95cd9af8b2827dcdfec6cb3d52b
Mr7fenix <mr7fenix@gmail.com>
2023-01-13 11:53:20 +0100

    Aggiungo un mapping per il percorso base

--- loyaltyprogrambackend/src/main/java/it/cs/unicam/dicygroup/loyaltyprogrambackend/LoyaltyprogrambackendApplication.java
+++ loyaltyprogrambackend/src/main/java/it/cs/unicam/dicygroup/loyaltyprogrambackend/LoyaltyprogrambackendApplication.java
@@ -2,12 +2,28 @@ package it.cs.unicam.dicygroup.loyaltyprogrambackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 @SpringBootApplication
 public class LoyaltyprogrambackendApplication {
+
+    @GetMapping("/")
+    String home() {
+        return "Questo è il programma per gestire vari programmi fedeltà. Il progetto è del gruppo DicyGroup e si trova su GitHub al link: https://github.com/Mr7fenix/LoyaltyProgram";
+    }
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(LoyaltyprogrambackendApplication.class, args);
     }
+
 
 }
