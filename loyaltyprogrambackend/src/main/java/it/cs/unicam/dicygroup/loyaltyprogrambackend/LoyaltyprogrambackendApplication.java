517519a55a1d730c3a53425cb5937c27928c0055
BuildTools <unconfigured@null.spigotmc.org>
2023-01-17 15:30:59 +0100

    Modified main

--- loyaltyprogrambackend/src/main/java/it/cs/unicam/dicygroup/loyaltyprogrambackend/LoyaltyprogrambackendApplication.java
+++ loyaltyprogrambackend/src/main/java/it/cs/unicam/dicygroup/loyaltyprogrambackend/LoyaltyprogrambackendApplication.java
@@ -2,13 +2,8 @@ package it.cs.unicam.dicygroup.loyaltyprogrambackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @SpringBootApplication
