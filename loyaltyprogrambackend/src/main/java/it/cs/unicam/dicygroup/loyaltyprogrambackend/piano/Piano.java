135a7c2aaa8408a3abf337f4cc016ba376cd7bd8
BuildTools <unconfigured@null.spigotmc.org>
2023-02-03 15:27:16 +0100

    Aggiunto json subtyping al piano

--- loyaltyprogrambackend/src/main/java/it/cs/unicam/dicygroup/loyaltyprogrambackend/piano/Piano.java
+++ loyaltyprogrambackend/src/main/java/it/cs/unicam/dicygroup/loyaltyprogrambackend/piano/Piano.java
@@ -1,5 +1,14 @@
 package it.cs.unicam.dicygroup.loyaltyprogrambackend.piano;
 
+import com.fasterxml.jackson.annotation.*;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = PianoLivelli.class, name = "PianoLivelli"),
+        @JsonSubTypes.Type(value = PianoPunti.class, name = "PianoPunti")
+})
 public interface Piano {
     public Long getId();
 
